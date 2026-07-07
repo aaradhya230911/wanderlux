4406f817ff1f95969b1beb3f0380a24680175573
aaradhya230911 <300708848+aaradhya230911@users.noreply.github.com>
2026-07-07 09:22:57 +0530

    Initial upload

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import * as React from "react";
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider attribute="class" defaultTheme="light" enableSystem {...props}>
       {children}
